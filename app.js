70590c2dc82aa5b4346b543803c838318d86deba
SirShagalot <56863638+SirShagalot@users.noreply.github.com>
2023-03-15 22:27:14 +0100

    first step

--- app.js
+++ app.js
@@ -3,11 +3,13 @@
 window.addEventListener("load", initApp);
 
 function initApp() {
-    //event listener for select on HTML
-    document.querySelector("#select-color-mode").addEventListener("change", modeSelected);
+  //event listener for select on HTML
+  document
+    .querySelector("#select-color-mode")
+    .addEventListener("change", modeSelected);
 }
 
 // modeSelected called when #select-color-mode changes value (the user select color mode)
 function modeSelected() {
-    console.log("New color mode selected");
+  console.log("New color mode selected");
 }
